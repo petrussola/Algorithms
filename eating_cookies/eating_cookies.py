aac91845ec3e5b34832f36b614ac2314c58e612f
Pere <pere.solaclaver@gmail.com>
2020-02-19 22:37:41 +0000

    Fibonacci on eating cookies problem with caching

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,16 +8,16 @@ import math
 # recursive solution
 
 
-def eating_cookies(n, cache=None):
-    # start at max number of cookies that it can eat (3) in the minimum amount of times i.e. for n = 9 it will be 3,3,3
-    # then reduce the number of cookies sequentially by adding more eating times if needed to reach n i.e. n = 9 3,2,3,1 or 2,3,3,1
-    # add each combination as a list inside the list as we go.
-    # return number of items in the list
-    if n == 2:
+def eating_cookies(n, cache):
+    if cache[n] > 0:
+        return cache[n]
+    elif n == 2:
         return 2
     elif n < 2:
         return 1
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    value = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    cache[n] = value
+    return value
 
 
 if __name__ == "__main__":
@@ -27,5 +27,3 @@ if __name__ == "__main__":
             ways=eating_cookies(num_cookies), n=num_cookies))
     else:
         print('Usage: eating_cookies.py [num_cookies]')
-
-# print(eating_cookies(5))
